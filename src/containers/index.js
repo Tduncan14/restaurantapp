cdc95f940a33f0ba64fae8bcfe3b4f9cefbb5cae
tduncan14 <nbatre1@yahoo.com>
2022-02-17 21:09:10 -0800

    keep moving forward

---
+++ src/containers/index.js
@@ -0,0 +1,20 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Intro from "./Intro/Intro";
+import Menu from "./Menu/Menu";
+import Laurels from "./Laurels/Laurels";
+import Findus from "./Findus/Findus";
+import Chef from './Chef/Chef';
+import AboutUs from "./AboutUs/AboutUs";
+
+
+
+
+
+
+
+
+export {Header,Footer,Intro,Menu,Laurels,Findus,Chef,AboutUs}
+
+
+
