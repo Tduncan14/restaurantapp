89816c8f3d4f90e7bddfc8f70af77e9a808ea150
tduncan14 <nbatre1@yahoo.com>
2022-02-24 01:26:38 -0800

    keep moving forward

--- src/containers/Laurels/Laurels.js
+++ src/containers/Laurels/Laurels.js
@@ -3,6 +3,29 @@ import { SubHeading } from '../../components';
 import {images,data} from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({award:{imgUrl,title,subtitle}}) => (
+  <div className="app__laurels_awards-card">
+
+    <img src={imgUrl}  alt="award"/>
+
+
+    <div  className="app__laurels_awards-card_content" >
+
+         <p className="p__cormorant" style={{color:'#DCCA87'}}>
+           {title}
+         </p>
+
+         <p className="p__cormorant">
+           {subtitle}
+         </p>
+
+    </div>
+
+
+   
+  </div>
+)
+
 const Laurels =()  =>{
   return (
 
@@ -12,6 +35,11 @@ const Laurels =()  =>{
          <SubHeading  title="Awards & recognition"/>
 
          <h1 className="headtext__cormorant">Our Laurels</h1>
+
+
+         <div className="app__laurels_awards">
+            {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+         </div>
 
        </div>
 
