d9fb734ac04a462d36197c71903a30a24f2b5ece
tduncan14 <nbatre1@yahoo.com>
2022-02-24 23:45:27 -0800

    keep moving forward

--- src/containers/Gallery/Gallery.js
+++ src/containers/Gallery/Gallery.js
@@ -54,9 +54,9 @@ const Gallery = () => {
 
            </div>
 
-           <div className="app__gallery-images_arrow">
+           <div className="app__gallery-images_arrows">
              <BsArrowLeftShort className='gallery__arrow-icon' onClick={()=> scroll('left')}/>
-             <BsArrowRightShort className='gallery__arrow-icon' onClick={()=> scroll('left')}/>
+             <BsArrowRightShort className='gallery__arrow-icon' onClick={()=> scroll('right')}/>
            </div>
        </div>
 
